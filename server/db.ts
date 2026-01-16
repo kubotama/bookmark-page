bf2cc4aac3d65e917685c34c8d55faca0b3f08fe
kubotama <16697573+kubotama@users.noreply.github.com>
2026-01-17 00:10:32 +0900

    feat: アプリケーション終了時にデータベースをクローズする処理を追加

--- server/db.ts
+++ server/db.ts
@@ -6,3 +6,12 @@ export const db = new Database(dbPath)
 
 // 開発中のデバッグ用に WAL モードなどを設定
 db.pragma('journal_mode = WAL')
+
+// アプリケーション終了時にデータベース接続を閉じる
+process.on('SIGINT', () => process.exit(0))
+process.on('SIGTERM', () => process.exit(0))
+process.on('exit', () => {
+  if (db.open) {
+    db.close()
+  }
+})
