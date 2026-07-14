961938194d2ce0a8f53571ce851b2dde38dd93f7
kubotama <16697573+kubotama@users.noreply.github.com>
2026-07-14 23:15:45 +0900

    feat: ブックマークの詳細ページの開くボタンを実装しました

--- src/components/BookmarkForm.tsx
+++ src/components/BookmarkForm.tsx
@@ -13,8 +13,25 @@ export const BookmarkForm = ({ bookmark }: BookmarkFormProps) => {
   const [title, setTitle] = useState(bookmark.title)
   const [url, setUrl] = useState(bookmark.url)
 
+  const isValidateUrl = () => {
+    try {
+      const newUrl = new URL(url)
+      // protocolには末尾に「:」が含まれるため、'http:' または 'https:' で判定します
+      return newUrl.protocol === 'http:' || newUrl.protocol === 'https:'
+    } catch {
+      // URLとして解析できない場合はfalse
+      return false
+    }
+  }
+
+  const isSubmitDisable = !isValidateUrl()
+
+  const handleSubmit = () => {
+    window.open(url, '_blank', 'noreferrer')
+  }
+
   return (
-    <div>
+    <form onSubmit={handleSubmit}>
       <div className="w-1/2 m-auto">
         <div className="grid grid-cols-[max-content_1fr] items-center gap-1">
           <label className="text-sm font-medium text-slate-600">
@@ -40,20 +57,35 @@ export const BookmarkForm = ({ bookmark }: BookmarkFormProps) => {
           />
         </div>
         <div className="mt-2 grid grid-cols-4 gap-2">
-          <button className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold">
+          <button
+            type="submit"
+            disabled={isSubmitDisable}
+            className={`border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer
+            hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold
+            disabled:bg-slate-200 disabled:text-slate-500`}
+          >
             {UI_LABELS.ACTIONS.OPEN}
           </button>
-          <button className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold">
+          <button
+            type="button"
+            className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold"
+          >
             {UI_LABELS.ACTIONS.UPDATE}
           </button>
-          <button className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold">
+          <button
+            type="button"
+            className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold"
+          >
             {UI_LABELS.ACTIONS.DELETE}
           </button>
-          <button className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold">
+          <button
+            type="button"
+            className="border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold"
+          >
             {UI_LABELS.ACTIONS.BACK}
           </button>
         </div>
       </div>
-    </div>
+    </form>
   )
 }
