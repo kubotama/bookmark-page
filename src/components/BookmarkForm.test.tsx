14e48ccbad4bde4af56e2ee4bbc81ac74c48139f
kubotama <16697573+kubotama@users.noreply.github.com>
2026-07-14 23:34:33 +0900

    feat: ブックマークの詳細ページの開くボタンを実装しました (#46)
    * feat: ブックマークの詳細ページの開くボタンを実装しました
    
    * fix: e.preventDefault() を呼び出してデフォルトの送信挙動をキャンセルするように修正しました
    
    * fix: window.open などのグローバルオブジェクトを beforeEach などのグローバルなテストセットアップフックでモック化することは避けて必要とする特定のテストケース内でのみ適用するように修正しました

---
+++ src/components/BookmarkForm.test.tsx
@@ -0,0 +1,98 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect, vi } from 'vitest'
+import { BookmarkForm } from './BookmarkForm'
+import { INVALID_STRING, TestBookmarks } from '../../functions/test/fixtures'
+import { UI_LABELS } from '../../shared/constants/uiMessages'
+import { userEvent } from '@testing-library/user-event'
+
+describe('BookmarkForm', () => {
+  it('該当するブックマークのurlとタイトルが正しく表示されること', async () => {
+    const targetBookmark = TestBookmarks[0]
+    render(<BookmarkForm key={targetBookmark.id} bookmark={targetBookmark} />)
+
+    const titleElement = await screen.findByRole('textbox', {
+      name: UI_LABELS.FIELDS.TITLE,
+    })
+    expect(titleElement).toHaveValue(targetBookmark.title)
+    const urlElement = await screen.findByRole('textbox', {
+      name: UI_LABELS.FIELDS.URL,
+    })
+    expect(urlElement).toHaveValue(targetBookmark.url)
+  })
+
+  describe('開くボタン', () => {
+    it('開くボタンをクリックしたときに、正しいURLと属性で window.open が呼び出されること', async () => {
+      const openSpy = vi.spyOn(window, 'open').mockImplementation(() => null)
+
+      const user = userEvent.setup()
+      const targetBookmark = TestBookmarks[0]
+
+      render(<BookmarkForm key={targetBookmark.id} bookmark={targetBookmark} />)
+
+      const openButton = screen.getByRole('button', {
+        name: UI_LABELS.ACTIONS.OPEN,
+      })
+
+      await user.click(openButton)
+
+      expect(openSpy).toHaveBeenCalledTimes(1) // 1回だけ呼ばれたか
+      expect(openSpy).toHaveBeenCalledWith(
+        targetBookmark.url,
+        '_blank', // 第2引数：ターゲット
+        'noreferrer', // 第3引数：機能文字列
+      )
+
+      openSpy.mockRestore()
+    })
+
+    it('URLを変更した場合には、変更したURLでwindow.open が呼び出されること', async () => {
+      const openSpy = vi.spyOn(window, 'open').mockImplementation(() => null)
+
+      const user = userEvent.setup()
+      const targetBookmark = TestBookmarks[0]
+      const inputUrl = TestBookmarks[1].url
+
+      render(<BookmarkForm key={targetBookmark.id} bookmark={targetBookmark} />)
+
+      const urlElement = await screen.findByRole('textbox', {
+        name: UI_LABELS.FIELDS.URL,
+      })
+      await user.clear(urlElement)
+      await user.type(urlElement, inputUrl)
+
+      const openButton = screen.getByRole('button', {
+        name: UI_LABELS.ACTIONS.OPEN,
+      })
+
+      await user.click(openButton)
+
+      expect(openSpy).toHaveBeenCalledTimes(1) // 1回だけ呼ばれたか
+      expect(openSpy).toHaveBeenCalledWith(
+        inputUrl,
+        '_blank', // 第2引数：ターゲット
+        'noreferrer', // 第3引数：機能文字列
+      )
+
+      openSpy.mockRestore()
+    })
+
+    it('URLのテキストボックスに不正な文字列が入力されている場合には開くボタンは無効となること', async () => {
+      const user = userEvent.setup()
+      const targetBookmark = TestBookmarks[0]
+
+      render(<BookmarkForm key={targetBookmark.id} bookmark={targetBookmark} />)
+
+      const urlElement = await screen.findByRole('textbox', {
+        name: UI_LABELS.FIELDS.URL,
+      })
+      await user.clear(urlElement)
+      await user.type(urlElement, INVALID_STRING.URL)
+
+      const openButton = screen.getByRole('button', {
+        name: UI_LABELS.ACTIONS.OPEN,
+      })
+
+      expect(openButton).toBeDisabled()
+    })
+  })
+})
