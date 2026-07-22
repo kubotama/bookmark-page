ebb2837cef6a23360f225ad82bd9a1e472db9cc5
kubotama <16697573+kubotama@users.noreply.github.com>
2026-07-22 16:48:01 +0900

    test: 異常系のテストを追加しました

--- src/hooks/useUpdateBookmark.test.tsx
+++ src/hooks/useUpdateBookmark.test.tsx
@@ -4,6 +4,8 @@ import { uuidv7 } from 'uuidv7'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { TestBookmarks } from '../../functions/test/fixtures'
 import { useUpdateBookmark } from './useUpdateBookmark'
+import { ERROR_MESSAGE, UI_MESSAGES } from '../../shared/constants/uiMessages'
+import { SCHEMA_MESSAGE } from '../../shared/constants/validation'
 
 const { mockPatch } = vi.hoisted(() => ({
   mockPatch: vi.fn(),
@@ -81,10 +83,91 @@ describe('useUpdateBookmark', () => {
       })
     })
   })
+
   describe('異常系', () => {
-    it('APIが400でバリデーションエラーを返したときにエラー処理が行われること', () => {})
-    it('APIが404でブックマークが存在しないエラーを返したときにエラー処理が行われること', () => {})
-    it('APIが409で登録済みのurlを登録しようとしたエラーを返したときにエラー処理が行われること', () => {})
-    it('APIが500などの一般的なエラーを返したときにエラー処理が行われること', () => {})
+    it.each([
+      {
+        status: 400,
+        errorName: 'バリデーション',
+        errorText: SCHEMA_MESSAGE.INVALID_ID_FORMAT,
+      },
+      {
+        status: 404,
+        errorName: 'ブックマークが存在しない',
+        errorText: UI_MESSAGES.API.NOT_FOUND_BOOKMARK,
+      },
+      {
+        status: 409,
+        errorName: '登録済みのurlを登録しようとした',
+        errorText: UI_MESSAGES.API.DUPLICATE_URL,
+      },
+      {
+        status: 500,
+        errorName: '一般的な',
+        errorText: ERROR_MESSAGE.SERVER_ERROR,
+      },
+    ])(
+      `APIが$statusで$errorNameエラーを返したときにエラー処理が行われること`,
+      async ({ status, errorText }) => {
+        mockPatch.mockResolvedValueOnce({
+          status,
+          ok: false,
+          json: async () => ({
+            success: false,
+            error: errorText,
+          }),
+        })
+        const updatedPayload = {
+          id: validId,
+          title: TestBookmarks[0].title,
+          url: TestBookmarks[0].url,
+        }
+        const { queryClient, wrapper } = createTestQueryClient()
+        const invalidateSpy = vi.spyOn(queryClient, 'invalidateQueries')
+
+        const { result } = renderHook(() => useUpdateBookmark(), { wrapper })
+
+        result.current.mutate(updatedPayload)
+
+        await waitFor(() => {
+          expect(result.current.isSuccess).toBe(false)
+          expect(result.current.error).toBeInstanceOf(Error)
+          expect(result.current.error?.message).toBe(errorText)
+
+          expect(invalidateSpy).not.toHaveBeenCalled()
+        })
+      },
+    )
+
+    it(`APIが不明なエラーを返したときにエラー処理が行われること`, async () => {
+      mockPatch.mockResolvedValueOnce({
+        status: 500,
+        ok: false,
+        json: async () => ({
+          success: false,
+        }),
+      })
+      const updatedPayload = {
+        id: validId,
+        title: TestBookmarks[0].title,
+        url: TestBookmarks[0].url,
+      }
+      const { queryClient, wrapper } = createTestQueryClient()
+      const invalidateSpy = vi.spyOn(queryClient, 'invalidateQueries')
+
+      const { result } = renderHook(() => useUpdateBookmark(), { wrapper })
+
+      result.current.mutate(updatedPayload)
+
+      await waitFor(() => {
+        expect(result.current.isSuccess).toBe(false)
+        expect(result.current.error).toBeInstanceOf(Error)
+        expect(result.current.error?.message).toBe(
+          ERROR_MESSAGE.FAILED_UPDATE_BOOKMARK,
+        )
+
+        expect(invalidateSpy).not.toHaveBeenCalled()
+      })
+    })
   })
 })
