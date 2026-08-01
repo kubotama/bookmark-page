fefdbe1e1c08e1de5b87fe7c7db056a0533bb3da
kubotama <16697573+kubotama@users.noreply.github.com>
2026-08-01 16:24:43 +0900

    feat: ボタンの共通機能を提供するButtonコンポーネントを作成しました

---
+++ src/components/Button.tsx
@@ -0,0 +1,19 @@
+import { cn } from '../../shared/lib/utils'
+
+export const Button: React.FC<
+  React.ButtonHTMLAttributes<HTMLButtonElement>
+> = ({ children, className, ...props }) => {
+  return (
+    <button
+      className={cn(
+        `border border-slate-300 text-slate-700 bg-indigo-200 rounded px-2 py-1 cursor-pointer
+            hover:text-slate-200 hover:bg-indigo-700 hover:font-semibold
+            disabled:bg-slate-200 disabled:text-slate-500 disabled:cursor-not-allowed`,
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
