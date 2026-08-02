4116f901b90733710ed7a054a0a055221b3d531d
kubotama <16697573+kubotama@users.noreply.github.com>
2026-08-02 15:47:10 +0900

    fix: タイトルのリンクをaタグからLinkに変更しました

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 
 import { UI_LABELS } from '../../shared/constants/uiMessages'
 
@@ -7,9 +7,7 @@ export const Route = createRootRoute({
     <div className="min-h-screen bg-slate-50 text-slate-900">
       {/* 必要であればここにヘッダーなどを共通配置 */}
       <div className="text-slate-200 bg-slate-700 font-bold w-1/2 m-auto text-center text-xl p-2 mb-2 mt-2">
-        <a href="/" rel="noreferrer">
-          {UI_LABELS.HEADER.PAGE_HEADER}
-        </a>
+        <Link to="/">{UI_LABELS.HEADER.PAGE_HEADER}</Link>
       </div>
 
       <main className="container mx-auto">
