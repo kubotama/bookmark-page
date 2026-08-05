ce11e3a75beb972b6f734f2d6c571135454d2881
kubotama <16697573+kubotama@users.noreply.github.com>
2026-08-05 16:46:53 +0900

    refact:[extension, shared] urlの検証を共有ライブラリに移動した
    検証をZodのスキーマを利用するように変更した。
    handleSaveApiUrlはカスタムフックに移すため、暫定的にテストをパスするように修正した。

--- shared/lib/utils.ts
+++ shared/lib/utils.ts
@@ -1,6 +1,19 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { BookmarkUrlSchema } from '../../functions/schemas/bookmark'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const validateUrl = (url: string) => {
+  return BookmarkUrlSchema.safeParse(url)
+}
+
+export class ValidationError extends Error {
+  constructor(message: string) {
+    super(message)
+    // エラーの名前をクラス名と一致させる
+    this.name = 'ValidationError'
+  }
 }
