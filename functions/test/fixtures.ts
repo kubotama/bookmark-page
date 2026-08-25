2af3ce42e27acbc0f1efcf8fb5d1946fcd4fdfa2
kubotama <16697573+kubotama@users.noreply.github.com>
2026-08-25 20:52:16 +0900

    refact: データベースの重複エラーのメッセージをブックマーク以外にも作成できるように修正した

--- functions/test/fixtures.ts
+++ functions/test/fixtures.ts
@@ -43,7 +43,7 @@ export const INVALID_STRING = {
 
 export const TEST_ERROR_MESSAGE = {
   API_ERROR: 'APIへのアクセスにエラーが発生しました',
-  CONSTRAINT_ERROR: 'UNIQUE constraint failed: bookmarks.url',
+  CONSTRAINT_BOOKMARK_ERROR: 'UNIQUE constraint failed: bookmarks.url',
   DB_ERROR: 'データベースにエラーが発生しました',
   NETWORK_ERROR: 'ネットワークにエラーが発生しました',
   SAVE_ERROR: '保存できませんでした',
